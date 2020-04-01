c178a44554ea98d05490d0deaa34f96e8856b883
RuiTung <ruimavic@gmail.com>
2020-04-01 00:33:14 -0700

    html, style update, add js and seal character

---
+++ index.js
@@ -0,0 +1,16 @@
+window.onscroll = function () {
+    this.goToTop();
+}
+function goToTop() {
+    var topBtn = document.getElementById("topBtn");
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+}
+
+function initTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
